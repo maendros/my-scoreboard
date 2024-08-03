d823aec32ed0f455f817760c7fa11e5500d17977
Paschalis Valtsianis <valitsianius@hotmail.com>
2024-08-03 18:16:49 +0300

    fix seeded match

--- prisma/seedMatch.ts
+++ prisma/seedMatch.ts
@@ -12,39 +12,9 @@ async function main() {
         homeScore: 1,
         awayScore: 3,
         playedAt: new Date("2024-08-03T14:37:18.337Z"),
-        userTeams: [
-          {
-            userId: 1,
-            teamId: 1,
-            isWinner: false,
-          },
-          {
-            userId: 3,
-            teamId: 2,
-            isWinner: true,
-          },
-        ],
       },
+      // Add more match objects here as needed
     ];
-
-    // Check if teams exist in the database
-    const existingTeams = await prisma.team.findMany();
-    console.log("Existing Teams in DB:", existingTeams);
-
-    // Mapping userTeams to check if teamId exists in the database
-    for (const match of matches) {
-      for (const userTeam of match.userTeams) {
-        const teamExists = existingTeams.some(
-          (team) => team.id === userTeam.teamId
-        );
-        if (!teamExists) {
-          console.error(
-            `Team with ID ${userTeam.teamId} does not exist in the database.`
-          );
-          return;
-        }
-      }
-    }
 
     // Iterate through matches and create them in the database
     for (const match of matches) {
@@ -55,13 +25,7 @@ async function main() {
           homeScore: match.homeScore,
           awayScore: match.awayScore,
           playedAt: match.playedAt,
-          userTeams: {
-            create: match.userTeams.map((userTeam) => ({
-              userId: userTeam.userId,
-              teamId: userTeam.teamId,
-              isWinner: userTeam.isWinner,
-            })),
-          },
+          // No need to include userTeams if not required
         },
       });
       console.log("Match created:", createdMatch);
