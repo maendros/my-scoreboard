88ffa3f9b760177fd89a8f265df38e8f82236ae2
Paschalis Valtsianis <valitsianius@hotmail.com>
2024-12-14 14:28:09 +0200

    update next.config timeout and football team image crests

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { isServer }) {
-      // Do not modify 'devtool' in development mode to avoid performance issues.
-      if (isServer) {
-        config.watchOptions = {
-          poll: 1000, // Check for changes every second
-          aggregateTimeout: 300, // Delay rebuild to avoid multiple restarts
-        };
-      }
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+  staticPageGenerationTimeout: 180, // 3 minutes
+  webpack(config, { isServer }) {
+    // Do not modify 'devtool' in development mode to avoid performance issues.
+    if (isServer) {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every second
+        aggregateTimeout: 300, // Delay rebuild to avoid multiple restarts
+      };
+    }
+
+    return config;
+  },
+  images: {
+    domains: ["crests.football-data.org"], // Add this if you're using team crests
+  },
+};
+
+export default nextConfig;
